265366954e409382591a9c280bc1f6ff9083c01d
pferrervich <pferrervich@protonmail.com>
2020-01-29 15:48:46 +0100

    Setup EventHandlers on SearchBar

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,8 +1,35 @@
 import React from "react";
 
 class SearchBar extends React.Component {
+  state = { term: "" };
+
+  /* Callback function that gets called with the event object and takes the value of the input 
+  out of the event and assigns it in the state property */
+  onInputChange = event => {
+    this.setState({ term: event.target.value });
+  };
+
+  onFormSubmit = event => {
+    //prevents browser from refreshing page on submit
+    event.preventDefault();
+    //TODO: Make sure we call callback from parent component
+  };
+
   render() {
-    return <div>Searchbar</div>;
+    return (
+      <div className="search-bar ui segment">
+        <form onSubmit={this.onFormSubmit} className="ui form">
+          <div className="field">
+            <label>Video Search!</label>
+            <input
+              onChange={this.onInputChange}
+              value={this.state.term}
+              type="text"
+            ></input>
+          </div>
+        </form>
+      </div>
+    );
   }
 }
 
