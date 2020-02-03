4260c3ed3c2146fdd9620a6f9ecb24d9560c04ac
pferrervich <pferrervich@protonmail.com>
2020-02-03 18:13:09 +0100

    added video iframe

--- src/components/VideoDetail.js
+++ src/components/VideoDetail.js
@@ -5,8 +5,15 @@ const VideoDetail = ({ video }) => {
   if (!video) {
     return <div>Loading...</div>;
   }
+
+  //                                    ES2015 template String
+  const videoSrc = `https://www.youtube.com/embed/${video.id.videoId}`;
+
   return (
     <div>
+      <div className="ui embed">
+        <iframe src={videoSrc} />
+      </div>
       <div className="ui segment">
         <h4 className="header">{video.snippet.title}</h4>
         <p>{video.snippet.description}</p>
