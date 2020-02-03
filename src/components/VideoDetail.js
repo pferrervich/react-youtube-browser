47fd1924109bbab357cb78c5e48e0ec3ac29d677
pferrervich <pferrervich@protonmail.com>
2020-02-03 18:24:06 +0100

    Fixed some errors

--- src/components/VideoDetail.js
+++ src/components/VideoDetail.js
@@ -12,7 +12,7 @@ const VideoDetail = ({ video }) => {
   return (
     <div>
       <div className="ui embed">
-        <iframe src={videoSrc} />
+        <iframe title="Video Player" src={videoSrc} />
       </div>
       <div className="ui segment">
         <h4 className="header">{video.snippet.title}</h4>
